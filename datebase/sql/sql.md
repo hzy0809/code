74568e7d7b8e7f5161d0126232ba5ac09aa98974
hzy <huzhenyu@aidigger.com>
2021-05-11 19:41:29 +0800

    sql.md

--- datebase/sql/sql.md
+++ datebase/sql/sql.md
@@ -21,21 +21,39 @@ WHERE TerritoryID IS NOT NULL
   AND SalesYTD <> 0;  
 ```
 
-+ 连续排序无跳跃
+#### 连续排序无跳跃DENSE_RANK
   ```sql
   DENSE_RANK ( ) OVER ( [ <partition_by_clause> ] < order_by_clause > )  
   ```
-+ 连续排序跳跃
+#### 连续排序跳跃RANK
   ```sql
   RANK ( ) OVER ( [ partition_by_clause ] order_by_clause )  
   ```
-+ 连续排序不重复
+#### 连续排序不重复ROW_NUMBER
   ```sql
   ROW_NUMBER ( ) OVER ( [ PARTITION BY value_expression , ... [ n ] ] order_by_clause )  
   ```
-+ 排序分层
+#### 排序分层NTILE
   ```sql
   NTILE (integer_expression) OVER ( [ <partition_by_clause> ] < order_by_clause > ) 
   ```
   PARTITION BY：划分组的依据   
   ORDER BY：排序依据
+  
+### 类型转换
+
+[DOC](https://docs.microsoft.com/en-us/sql/t-sql/functions/cast-and-convert-transact-sql?view=sql-server-ver15)
+#### CONVERT
+```sql
+CONVERT ( data_type [ ( length ) ] , expression [ , style ] ) 
+```
+#### CAST
+```sql
+CAST ( expression AS data_type [ ( length ) ] ) 
+```
+
+### 精度转换
+#### ROUND
+```sql
+ROUND ( numeric_expression , length [ ,function ] ) 
+```
--- python/usage/OOP/operation_overload.py
+++ python/usage/OOP/operation_overload.py
@@ -12,21 +12,6 @@ class Instance:
         """
         self.data = data
 
-    def __sub__(self, other):
-        """
-        捕捉减法表达式
-        :param other:
-        :return:
-        """
-        return Instance(self.data - other)
-
-    def __del__(self):
-        """
-        析构函数，对象回收
-        :return:
-        """
-        pass
-
     def __getitem__(self, item):
         """
         索引x[i]、切片x[i:j]以及迭代（in，map，for，类型构造list等，列表解析，列表和元组的赋值运算）
@@ -58,3 +43,151 @@ class Instance:
         :return:
         """
         pass
+
+    def __contains__(self, item):
+        """
+        in 通常实现为迭代
+        __contains__优先于__iter__
+        __iter__优先于__getitem__
+        :param item:
+        :return:
+        """
+        return item in self.data
+
+    def __getattr__(self, item):
+        """
+        拦截未定义的属性或者实例的点号运算
+        :param item:string, attr name
+        :return:
+        """
+        pass
+
+    def __getattribute__(self, item):
+        """
+
+        :param item:
+        :return:
+        """
+        pass
+
+    def __setattr__(self, key, value):
+        """
+        拦截所有属性的赋值语句
+        :param key:
+        :param value:
+        :return:
+        """
+        self.__dict__[key] = value
+
+    def __repr__(self):
+        """
+        适用于一切环境，优先级低于__str__
+        :return:string
+        """
+        pass
+
+    def __str__(self):
+        """
+        响应print和str
+        :return:string
+        """
+        pass
+
+    def __add__(self, other):
+        """
+        不支持+运算符右侧使用实例对象
+        :param other:
+        :return:
+        """
+        return self.data + other
+
+    def __radd__(self, other):
+        """
+        只有当右侧使用实例对象，左侧不使用实例对象时调用
+        :param other:
+        :return:
+        """
+        return other + self.data
+
+    def __iadd__(self, other):
+        """
+        原处修改，优先级高于__add__
+        :param other:
+        :return:
+        """
+        pass
+
+    def __sub__(self, other):
+        """
+        捕捉减法表达式
+        :param other:
+        :return:
+        """
+        return Instance(self.data - other)
+
+    def __call__(self, *args, **kwargs):
+        """
+        为实例应用函数表达式提供支持,实现函数回调，或者函数绑定
+        装饰器
+        :param args:
+        :param kwargs:
+        :return:
+        """
+
+    def __gt__(self, other):
+        """
+        __lt__,__eq__,__ne__等
+        没有隐式关系，==并不意味!=是假
+        :param other:
+        :return:
+        """
+        return self.data > other
+
+    def __bool__(self):
+        """
+        获取bool值优先使用__bool__，如果未定义，则使用__len__,如果__len__未定义，则返回True
+        :return:
+        """
+        pass
+
+    def __len__(self):
+        """
+
+        :return:
+        """
+        pass
+
+    def __del__(self):
+        """
+        析构函数，对象回收
+        :return:
+        """
+        pass
+
+
+class Commuter(object):
+    def __init__(self, val):
+        self.val = val
+
+    def __add__(self, other):
+        if isinstance(other, Commuter):
+            other = other.val
+        return Commuter(self.val + other)
+
+    def __radd__(self, other):
+        return Commuter(other + self.val)
+
+    def __gt__(self, other):
+        return self.val > other
+
+
+
+if __name__ == '__main__':
+    c1 = Commuter(1)
+    c2 = Commuter(2)
+    c3 = c1 + c2
+    # __add__不进行类型判断时，c3.val是一个Commuter对象
+    print(c3.val)
+
+    cb3 = (lambda color='red': 'turn ' + color)
+    print(cb3('blue'))
