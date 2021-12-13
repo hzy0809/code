b59c11d9da848b6a4fd5b14fedb27ebe58d7aca3
hzy <huzhenyu@aidigger.com>
2021-12-13 11:26:50 +0800

    chaos

--- python/usage/tools/logging_usage.py
+++ python/usage/tools/logging_usage.py
@@ -11,6 +11,17 @@ import logging
 # logging.basicConfig(level=logging.INFO, format="%(asctime)s;%(levelname)s;%(message)s")
 
 # 下方语句对该module生效
+"""
+%(pathname)s Full pathname of the source file where the logging call was issued(if available).
+
+%(filename)s Filename portion of pathname.
+
+%(module)s Module (name portion of filename).
+
+%(funcName)s Name of function containing the logging call.
+
+%(lineno)d Source line number where the logging call was issued (if available).
+"""
 logger = logging.getLogger(__name__)
 formatter = logging.Formatter("%(asctime)s;%(levelname)s;%(message)s")
 ch = logging.StreamHandler()
